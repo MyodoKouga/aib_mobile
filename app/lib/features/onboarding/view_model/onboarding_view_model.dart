c75e176bfdb770c9670d2fa45c83a448597910e0
livra-iizuka <iizuka.yuto@li-vra.co.jp>
2025-01-14 12:37:56 +0900

    import追加

--- app/lib/features/onboarding/view_model/onboarding_view_model.dart
+++ app/lib/features/onboarding/view_model/onboarding_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:app/features/onboarding/model/onboarding_content.dart';
 
 final onboardingProvider = StateNotifierProvider<OnboardingViewModel, int>((ref) {
   return OnboardingViewModel();
